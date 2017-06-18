5ecf077fad2f5a9f4fc6586531b13b96872b7258
gouravm <gouravm@users.noreply.github.com>
2017-06-17 23:33:42 -0700

    Buy Sell Stock coding exercise.

--- buysellstock.py
+++ buysellstock.py
@@ -1,12 +1,12 @@
-#Find the max profit on sale of the stocks from list
-#in the list [12,11,15,3,10]
+# Find the max profit on sale of the stocks from list
+# in the list [12,11,15,3,10]
 # max profit=10-3=>7 where 3 is purchase price and
 # 10 is selling price
 
-#Pseudo code:
-#set a pointer at 0th element as purchase price
-#set another pointer at 1st element as selling price
-#iterate O(n) time complexity to find the best sp-pp difference
+# Pseudo code:
+# set a pointer at 0th element as purchase price
+# set another pointer at 1st element as selling price
+# iterate O(n) time complexity to find the best sp-pp difference
 
 def findthemaxprofit(myl):
     maxprofit=0
